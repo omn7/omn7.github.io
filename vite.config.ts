209b4365922ddbc42a86736a34397cc6ce1a03ff
Om Narkhede <developers.omn@gmail.com>
2025-07-19 22:18:06 +0530

    commited code

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/",
+  base: process.env.VITE_BASE_PATH || '/portfolio',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
